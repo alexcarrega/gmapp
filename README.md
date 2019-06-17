418d3784cb31f75ca14b04e0a0eaf52383270f09
Alex Carrega <alessandro.carrega@cnit.it>
2019-06-17 15:51:58 +0200

    Minor changes

--- README.md
+++ README.md
@@ -1,60 +1,41 @@
-# WSCC (Wazapp Custom Client)
+# GMApp (Gmail App for Desktop)
 
-### **It is not an official application of WhatsApp Inc.**
+### **It is not an official application of Google Inc.**
 
-Build your own custom client for WhatsApp.
-
-Unfortunately there is no official Whatsapp client for Linux operating systems. But we are a Linux users, if it is not exists, we will create it.
-
+Build your own custom client for Gmail.
 This is created with [Electron](https://electronjs.org/) v4.
-
-## Changelog:
-
-### v3.0.0
-
-### Features
-1. Custom styles, you can make your own style or you can apply any of [usertstyles.com](https://userstyles.org/styles/browse?search_terms=whatsapp&type=false).
-2. If you have a version error with your browser, the program will solve it automatically.
-3. Dependency node-notifier was removed.
-4. Electron has been updated to version 4.
-5. All builds will be saved in the release folder.
-
-### Environment for dev
-1. main.js has been renamed to index.js
-2. app has been renamed to assets
-3. .gitignore has been added
 
 ## How to use
 
 Clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
 ```bash
-# Clone this repository
-git clone https://github.com/miccar/wazapp
-# Go into the repository
-cd wazapp
-# Install dependencies
+git clone https://github.com/alexcarrega/gmapp
+cd gmapp
 npm install
-# Run the app
 npm start
 ```
 
-## Create linux executable file
+## Create Linux executable file
+
+### 64 bit version
+
 ```bash
-# Create package for linux 64bit
 npm run build-linux64
-# Create package for linux 32bit
+```
+
+### 32 bit version
+
+```bash
 npm run build-linux32
 ```
-This command will create a folder called Wazapp-linux32(64) that contains the executable file. Now you have to include this file as a startup application of your Linux operating system 
+
+This command will create a folder called **gmapp-linux-x64** (**gmapp-linux-x32**) that contains the executable file. Now you have to include this file as a startup application of your Linux operating system 
 
 ## NOTE
+
 If it is not already installed, you have to include electron-packager for creating the linux executable file with the following command: 
 
 ```bash
-#install the dependency 
 npm install electron-packager
 ```
-## WazApp on Ubuntu 18.04
-The app starts in Hide mode(a background mode, only the icon on the panel is showed and the new message will be notified).
-Clicking on the _Show_ item, the app will be open.
- ![alt text](https://github.com/miccar/wazapp/blob/2%2C0/images/wazapp.jpg)
--- index.js
+++ index.js
@@ -8,31 +8,23 @@ const { app, BrowserWindow, Menu, Tray } = require('electron')
 const path = require('path')
 const got_the_lock = app.requestSingleInstanceLock();
 
-var wscc = null, tray = null, data = {
+var gmapp = null, tray = null, data = {
 	loaded: false
 };
-
-app.launcher.setBadgeCount = (count) => {
-	data.msg_count = count; 
-}
-app.launcher.getBadgeCount = () => data.msg_count;
 
 if (!got_the_lock) {
 	return app.quit()
 }
 
-// Second instance behaviour
 app.on('second-instance', (commandLine, workingDirectory) => {
-	// Someone tried to run a second instance, we should focus our window.
-	if (wscc.window) {
-		if (wscc.window.isMinimized()) {
-			wscc.window.restore();
+	if (gmapp.window) {
+		if (gmapp.window.isMinimized()) {
+			gmapp.window.restore();
 		}
-		wscc.window.show();
+		gmapp.window.show();
 	}
 })
 
-// Panel icon
 app.on('ready', () => {
 	['show_notifications', 'close_to_tray', 'start_in_tray'].forEach((set_prop) => {
 		if (!settings.has(set_prop)) {
@@ -48,10 +40,10 @@ app.on('ready', () => {
 	const contextMenu = Menu.buildFromTemplate([{
 			label: 'Toggle window show/hide',
 			click: () => {
-				if (wscc.isVisible()) {
-					wscc.hide();
+				if (gmapp.isVisible()) {
+					gmapp.hide();
 				} else {
-					wscc.show();
+					gmapp.show();
 				}
 			}
 		}, {
@@ -75,7 +67,19 @@ app.on('ready', () => {
 			click: (menu_item) => {
 				settings.set('start_in_tray', menu_item.checked);
 			}
-		},{
+      }, {
+         label: 'Swow Development Tools',
+         type: 'checkbox',
+         checked: settings.get('show_dev_tools'),
+         click: (menu_item) => {
+            settings.set('show_dev_tools', menu_item.checked);
+            if (menu_item.checked) {
+               gmapp.openDevTools();
+            } else {
+               gmapp.closeDevTools();
+            }
+         }
+      }, {
 			label: 'Quit',
 			click: () => {
 				app.isQuiting = true;
@@ -83,36 +87,26 @@ app.on('ready', () => {
 			}
 		}
 	])
-	tray.setToolTip('WazApp.')
+	tray.setToolTip('Gmapp')
 	tray.setContextMenu(contextMenu)
 	tray.on("double-click", function(event){
-		wscc.show();
+		gmapp.show();
 	})
 })
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-	// On macOS it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== 'darwin') {
 		app.quit()
 	}
 })
 
 app.on('activate', function () {
-	// On macOS it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (wscc === null) {
+	if (gmapp === null) {
 		createWindow()
 	}
 })
-
-// Utility functions
 
 global.osLinux = function (callback) {
 	if (process.platform === 'linux') {
@@ -125,10 +119,9 @@ function createWindow() {
 	var mainScreen = electron.screen.getPrimaryDisplay();
 	var dims = mainScreen.workAreaSize;
 
-	// Create the browser window.
-	wscc = new BrowserWindow({
+	gmapp = new BrowserWindow({
 		backgroundColor: '#2c2c2c',
-		title: "WhatsApp",
+		title: "GMail",
 		width: dims.width * .8,
 		height: dims.height * .8,
 		autoHideMenuBar: true,
@@ -139,57 +132,55 @@ function createWindow() {
 		show: !settings.get('start_in_tray')
 	})
 
-	// wscc.setResizable(false);
-	wscc.setMenuBarVisibility(false);
+	gmapp.setMenuBarVisibility(false);
 
-	// Set user agent of browser #avoid whatsapp error on chromium browser
-	wscc.webContents.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36")
-	wscc.loadURL("https://web.whatsapp.com");
+	gmapp.webContents.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)  Chrome/73.0.3683.86 Safari/537.36")
+	gmapp.loadURL("https://www.gmail.com");
 
-	// Notifications
-	wscc.on('page-title-updated', osLinux((event, title) => {
+   gmapp.on('page-title-updated', osLinux((event, title) => {
 		var msg_count = title.match(/\((\d+)\)/);
 		msg_count = msg_count ? msg_count[1] : '';
 		data.msg_count = parseInt(msg_count);
 		if (data.msg_count > 0) {
-			tray.setImage(path.join(__dirname, 'assets/icons/icon-msg.png'));
-			wscc.setIcon(path.join(__dirname, 'assets/icons/icon-msg.png'));
+			tray.setImage(path.join(__dirname, 'assets/icons/icon-mail.png'));
+			gmapp.setIcon(path.join(__dirname, 'assets/icons/icon-mail.png'));
 		} else {
 			if (data.content_loaded) {
 				tray.setImage(path.join(__dirname, 'assets/icons/icon.png'));
-				wscc.setIcon(path.join(__dirname, 'assets/icons/icon.png'));
+				gmapp.setIcon(path.join(__dirname, 'assets/icons/icon.png'));
 			} else {
 				tray.setImage(path.join(__dirname, 'assets/icons/icon-load.png'));
-				wscc.setIcon(path.join(__dirname, 'assets/icons/icon-load.png'));
+				gmapp.setIcon(path.join(__dirname, 'assets/icons/icon-load.png'));
 			}
 		}
 	}))
 
-	wscc.on('close', function (event) {
+	gmapp.on('close', function (event) {
 	    if(!app.isQuiting && settings.get('close_to_tray')){
 			event.preventDefault();
-			wscc.hide();
+			gmapp.hide();
 		}
 		return !settings.get('close_to_tray');
 	})
 
-	wscc.on('closed', function (event) {
-		wscc = null;
+	gmapp.on('closed', function (event) {
+		gmapp = null;
 	})
 
-	wscc.webContents.on('dom-ready', function (e) {
+	gmapp.webContents.on('dom-ready', function (e) {
 		let js_content = fs.readFileSync(path.join(__dirname, 'assets/init.js'), 'utf8')
 		let css_content = fs.readFileSync(path.join(__dirname, 'assets/styles.css'), 'utf8')
 		js_content = js_content.replace('{MY_CUSTOM_STYLE}', '`' + css_content + '`')
-		wscc.webContents.executeJavaScript(js_content);
+		gmapp.webContents.executeJavaScript(js_content);
 	})
 
-	// Open the DevTools.
-	// wscc.webContents.openDevTools()
-
-	wscc.webContents.on('did-finish-load', function () {
+	gmapp.webContents.on('did-finish-load', function () {
 		data.content_loaded = true;
 		tray.setImage(path.join(__dirname, 'assets/icons/icon.png'));
-		wscc.setIcon(path.join(__dirname, 'assets/icons/icon.png'));
+		gmapp.setIcon(path.join(__dirname, 'assets/icons/icon.png'));
 	})
+   
+   if (settings.get('show_dev_tools')) {
+      gmapp.openDevTools();
+   }
 }
